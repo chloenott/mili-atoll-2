cd288e85d49ad4936b351089f8c487f9cc9b89da
Chloe Nott <89756479+chloenott@users.noreply.github.com>
2023-08-19 09:52:21 -0700

    It appears the style transfer from image to video idea may have potential in using a postprocess shader with gradient kernel to match with closest matching pixel within input image, at low resolution. I think conceptually its crudely working, and hoping multiple comparison images will not cost too much. Very crudely working though

--- app/ocean_navigation/post_process.tsx
+++ app/ocean_navigation/post_process.tsx
@@ -13,55 +13,103 @@ const vertexShader = `
 const fragmentShader = `
   uniform sampler2D lowResScreen;
   uniform sampler2D highResScreen;
+  uniform sampler2D lowResOc1; // this needs to be in the same resolution as lowResScreen
   uniform sampler2D wc1;
   varying vec2 vUv;
 
-  vec2 gradient(sampler2D inputTexture, vec2 uv) {
-    float step = 0.01;
+  vec3 gradient(sampler2D inputTexture, vec2 uv, float step) {
     vec2 dx = vec2(step, 0.0);
     vec2 dy = vec2(0.0, step);
     float gradient_x = texture2D(inputTexture, uv + dx).b - texture2D(inputTexture, uv - dx).b;
     float gradient_y = texture2D(inputTexture, uv + dy).b - texture2D(inputTexture, uv - dy).b;
-    return vec2(gradient_x, gradient_y);
+    float gradient_z = 2.*texture2D(inputTexture, uv).b;
+    return vec3(gradient_x, gradient_y, gradient_z);
+  }
+
+  vec4 screenBlend(vec4 base, vec4 blend) {
+    return 1.0 - (1.0 - blend) * (1.0 - base);
   }
 
   void main() {
-    vec2 currentPixelGradient = gradient(lowResScreen, vUv);
+    vec3 currentPixelGradient = gradient(lowResScreen, vUv, 0.01);
+    float searchRadiusIncrements = 0.1;
 
+    float bestScore = 1.;
+    vec2 bestScoreUv = vUv;
     for (float i = -2.; i < 3.; i++) {
       for (float j = -2.; j < 3.; j++) {
-        vec2 uv = vUv + vec2(i/25., j/25.);
-        vec2 wc1_gradient = gradient(wc1, uv);
-        float weight = 1. - length(wc1_gradient - currentPixelGradient);
-        gl_FragColor += texture2D(wc1, uv) * weight;
-
+        vec2 uv2 = vUv + vec2(i*searchRadiusIncrements, j*searchRadiusIncrements);
+        vec3 oc1_gradient = gradient(lowResOc1, uv2, 0.01);
+        float score_LowerIsBetter = abs(length(oc1_gradient - currentPixelGradient));
+        if (score_LowerIsBetter < bestScore) {
+          bestScore = score_LowerIsBetter;
+          bestScoreUv = uv2;
+        }
       }
     }
+    
+    gl_FragColor = screenBlend(texture2D(highResScreen, vUv), texture2D(wc1, bestScoreUv));
+  }
+`
+
+const vertexShaderLowResOc1 = ` 
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`
+
+const fragmentShaderLowResOc1 = `
+  uniform sampler2D oc1; // this needs to be in the same resolution as lowResScreen
+  varying vec2 vUv;
+
+  void main() {
+    gl_FragColor = texture2D(oc1, vUv);
   }
 `
 
 export function PostProcess() {
   const { gl, size } = useThree();
 
-  const wc1 = useTexture('/images/wc1.jpeg')
+  const oc1 = useTexture('/images/oc1.jpg')
+  const wc1 = useTexture('/images/wc1.jpg')
+
   const geometry = new PlaneGeometry(size.width/250, size.height/250);
   const material = new ShaderMaterial({
     uniforms: {
       lowResScreen: new Uniform(null),
       highResScreen: new Uniform(null),
+      lowResOc1: new Uniform(null),
       wc1: new Uniform(wc1),
     },
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
   });
   const mesh = new Mesh(geometry, material);
-  const lowResScreenRenderTarget = new WebGLRenderTarget(size.width/10, size.height/10);
+
+  const lowResOcMaterial = new ShaderMaterial({
+    uniforms: {
+      oc1: new Uniform(oc1),
+    },
+    vertexShader: vertexShaderLowResOc1,
+    fragmentShader: fragmentShaderLowResOc1,
+  });
+  const lowResOcGeometry = new PlaneGeometry(size.width/250, size.height/250);
+  const lowResOcMesh = new Mesh(lowResOcGeometry, lowResOcMaterial);
+
+  const lowResScreenRenderTarget = new WebGLRenderTarget(100, 100);
+  const lowResOC1RenderTarget = new WebGLRenderTarget(100, 100);
   const highResScreenRenderTarget = new WebGLRenderTarget(size.width, size.height);
 
   useFrame(({gl, scene, camera}) => {
     gl.setRenderTarget(lowResScreenRenderTarget);
     gl.render(scene, camera);
     material.uniforms.lowResScreen.value = lowResScreenRenderTarget.texture;
+
+    gl.setRenderTarget(lowResOC1RenderTarget);
+    gl.render(lowResOcMesh, camera);
+    material.uniforms.lowResOc1.value = lowResOC1RenderTarget.texture;
 
     gl.setRenderTarget(highResScreenRenderTarget);
     gl.render(scene, camera);
