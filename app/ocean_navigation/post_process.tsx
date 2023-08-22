b93db9953a7f3a148c5e7790c94b39ada8a5339c
Chloe Nott <89756479+chloenott@users.noreply.github.com>
2023-08-21 18:19:40 -0700

    Forgot a step. Uses only the red channel to determine edge intensity, then multiplies by directional color for intermediate visualization showing typically sharper leading edge is most prominent feature with apparently correct direction detected

--- app/ocean_navigation/post_process.tsx
+++ app/ocean_navigation/post_process.tsx
@@ -1,8 +1,9 @@
 import { useTexture } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { Mesh, PlaneGeometry, ShaderMaterial, Uniform, WebGLRenderTarget } from "three";
+import { FloatType, Mesh, NearestFilter, PlaneGeometry, RedFormat, ShaderMaterial, Texture, Uniform, UnsignedByteType, WebGLRenderTarget } from "three";
+import { texture } from "three/examples/jsm/nodes/Nodes.js";
 
-const vertexShader = ` 
+const vertexShader = `
   varying vec2 vUv;
   void main() {
     vUv = uv;
@@ -14,36 +15,36 @@ const fragmentShader = `
   uniform sampler2D initialScene;
   varying vec2 vUv;
 
-  vec4 applyGaussianKernel(sampler2D inputTexture, vec2 uv) {
+  float applyGaussianKernel(sampler2D inputTexture, vec2 uv) {
     vec2 texelSize = 1.0 / vec2(textureSize(inputTexture, 0));
     mat3 kernel = mat3(
       1.0, 2.0, 1.0,
       2.0, 4.0, 2.0,
       1.0, 2.0, 1.0
     ) / 16.0;
-    vec4 result = vec4(0.0);
+    float result = 0.;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
             vec2 offset = vec2(float(i), float(j)) * texelSize;
-            vec4 sampled = texture(inputTexture, uv + offset);
+            float sampled = texture(inputTexture, uv + offset).x;
             result += sampled * kernel[i + 2][j + 2];
         }
     }
     return result;
   }
 
-  vec4 applyEdgeDetectionKernel(sampler2D inputTexture, vec2 uv) {
+  float applyEdgeDetectionKernel(sampler2D inputTexture, vec2 uv) {
     vec2 texelSize = 1.0 / vec2(textureSize(inputTexture, 0));
     mat3 kernel = mat3(
       -1.0, -1.0, -1.0,
       -1.0,  8.0, -1.0,
       -1.0, -1.0, -1.0
   );
-    vec4 result = vec4(0.0);
+    float result = 0.;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
             vec2 offset = vec2(float(i), float(j)) * texelSize;
-            vec4 sampled = texture(inputTexture, uv + offset);
+            float sampled = texture(inputTexture, uv + offset).x;
             result += sampled * kernel[i + 2][j + 2];
         }
     }
@@ -58,17 +59,16 @@ const fragmentShader = `
   vec4 getEdgeDirection(sampler2D inputTexture, vec2 uv) {
     vec2 texelSize = 1.0 / vec2(textureSize(inputTexture, 0));
     float gradientX = texture2D(inputTexture, vec2(uv.x+texelSize.x,uv.y)).x - texture2D(inputTexture, vec2(uv.x-texelSize.x,uv.y)).x;
-    float gradientY = texture2D(inputTexture, vec2(uv.x,uv.y+texelSize.y)).y - texture2D(inputTexture, vec2(uv.x,uv.y-texelSize.y)).y;
+    float gradientY = texture2D(inputTexture, vec2(uv.x,uv.y+texelSize.y)).x - texture2D(inputTexture, vec2(uv.x,uv.y-texelSize.y)).x;
     float angle = atan(gradientY, gradientX);
     vec4 color = vec4(hsvToRgb(vec3(angle/6.28, 1.0, 1.0)), 1.0);
     return color;
   }
 
   void main() {
-    //vec4 color = applyGaussianKernel(initialScene, vUv);
-    //vec4 color = applyEdgeDetectionKernel(initialScene, vUv);
-    vec4 color = getEdgeDirection(initialScene, vUv);
-    gl_FragColor = color; //texture2D(initialScene, vUv);
+    float intensity = applyEdgeDetectionKernel(initialScene, vUv);
+    vec4 color = 200.*intensity*getEdgeDirection(initialScene, vUv);
+    gl_FragColor = color;texture2D(initialScene, vUv);
   }
 `
 
@@ -76,20 +76,21 @@ export function PostProcess() {
   const { gl, size } = useThree();
 
   const aspectRatio = size.width / size.height;
-  const oc1 = useTexture('/images/oc1.jpg')
-  const wc1 = useTexture('/images/wc1.jpg')
+  const pixelRatio = devicePixelRatio;
+  const initTexture = new Texture();
 
   const geometry = new PlaneGeometry(size.width/250, size.height/250);
   const material = new ShaderMaterial({
     uniforms: {
-      initialScene: new Uniform(null),
-      wc1: new Uniform(wc1),
+      initialScene: {value: initTexture},
     },
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
   });
   const mesh = new Mesh(geometry, material);
-  const initialScene = new WebGLRenderTarget(size.width, size.height);
+  console.log(gl.info)
+  const initialScene = new WebGLRenderTarget(size.width*pixelRatio*4, size.height*4)
+  initialScene.texture.format = RedFormat;
 
   useFrame(({gl, scene, camera}) => {
     gl.setRenderTarget(initialScene);
--- app/ocean_navigation/r3f_canvas.tsx
+++ app/ocean_navigation/r3f_canvas.tsx
@@ -8,7 +8,7 @@ import { toneMapping } from 'three/examples/jsm/nodes/Nodes.js'
 
 export default function R3fCanvas() {
   return (
-    <Canvas flat={true} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
+    <Canvas flat={true} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }} gl={{toneMapping: NoToneMapping, precision: 'highp', antialias: true}}>
       <Scene />
       <Stats />
     </Canvas>
